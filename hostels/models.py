1b0292dc6d61faf090a59c8eb3ed1c45a10ffc6d
reubennkukuu08 <95329726+reubennkukuu08@users.noreply.github.com>
2022-02-23 02:00:27 +0300

    Update models.py

--- hostels/models.py
+++ hostels/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=100)
-    details = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
     price = models.CharField(max_length = 20)
